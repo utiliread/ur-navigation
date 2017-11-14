0616c8f898e33f85f2fba85a9b8b9464a0cef3ef
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2017-11-14 13:53:02 +0100

    fix(index) add nav-breadcrumbs custom element

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
 
 export function configure(config: FrameworkConfiguration) {
     config.globalResources([
+        PLATFORM.moduleName('./nav-breadcrumbs'),
         PLATFORM.moduleName('./nav-active'),
         PLATFORM.moduleName('./nav-href'),
         PLATFORM.moduleName('./nav-toggle')
