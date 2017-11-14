2fe3a9d034df56701bb5108b66f17bc0566f32f5
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2017-11-14 13:56:23 +0100

    fix(dist) include last change in dist output

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 import { PLATFORM } from 'aurelia-framework';
 export function configure(config) {
     config.globalResources([
+        PLATFORM.moduleName('./nav-breadcrumbs'),
         PLATFORM.moduleName('./nav-active'),
         PLATFORM.moduleName('./nav-href'),
         PLATFORM.moduleName('./nav-toggle')
